1b36cdcefe5f9059ed1b2ae3ebd82460de14472f
khannsameer <sk8717108@gmail.com>
2026-02-07 17:55:14 +0530

    Update registration action logic

--- src/app/register/registrationAction.action.tsx
+++ src/app/register/registrationAction.action.tsx
@@ -3,6 +3,7 @@
 import { db } from "@/config/db";
 import { users } from "@/drizzle/schema";
 import argon2 from "argon2";
+import { eq, or } from "drizzle-orm";
 
 export const registrationAction = async (registrationData: {
   name: string;
@@ -13,6 +14,24 @@ export const registrationAction = async (registrationData: {
 }) => {
   try {
     const { name, userName, email, password, role } = registrationData;
+
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(or(eq(users.email, email), eq(users.userName, userName)));
+
+    if (user) {
+      if (user.email === email)
+        return {
+          status: "ERROR",
+          message: "Email Already Exists",
+        };
+      else
+        return {
+          status: "ERROR",
+          message: "UserName Already Exists",
+        };
+    }
 
     const hashPassword = await argon2.hash(password);
 
