b4ea40d3725b350ce4536e3fa9d5c725aaef8a6e
khannsameer <sk8717108@gmail.com>
2026-02-10 18:05:11 +0530

    Update employer dashboard and home page, and add employer stats component

--- src/app/employer-dashboard/page.tsx
+++ src/app/employer-dashboard/page.tsx
@@ -1,9 +1,25 @@
-import { logoutUserAction } from "@/features/server/auth.actions";
+import StatsCard from "@/features/employers/components/employer-stats";
+import { getCurrentUser } from "@/features/server/auth.queries";
+import { redirect } from "next/navigation";
 
-const EmployerDashboard = () => {
+const EmployerDashboard = async () => {
+  const user = await getCurrentUser();
+  console.log("users data employer", user);
+
+  if (!user) return redirect("/login");
+
   return (
-    <div>
-      <h1>Employer Dashboard</h1>
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-2xl font-semibold text-foreground">
+          Hello, <span className="capitalize">{user?.name.toLowerCase()}</span>
+        </h1>
+        <p className="text-muted-foreground">
+          Here is your daily activities and application
+        </p>
+      </div>
+
+      <StatsCard />
     </div>
   );
 };
