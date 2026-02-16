e2da4a974cc8a7275bddfb86e9c2608c77fdce59
khannsameer <sk8717108@gmail.com>
2026-02-16 15:27:35 +0530

    Update constants and database schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,11 @@
+import {
+  JOB_LEVEL,
+  JOB_TYPE,
+  MIN_EDUCATION,
+  SALARY_CURRENCY,
+  SALARY_PERIOD,
+  WORK_TYPE,
+} from "@/config/constant";
 import { relations } from "drizzle-orm";
 import {
   int,
@@ -8,7 +16,7 @@ import {
   mysqlEnum,
   year,
   date,
-  serial,
+  boolean,
 } from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable("users", {
@@ -80,6 +88,39 @@ export const applicants = mysqlTable("applicants", {
   createdAt: timestamp("created_at", { mode: "string" }).defaultNow().notNull(),
   updatedAt: timestamp("updated_at", { mode: "string" }).defaultNow().notNull(),
 });
+
+export const jobs = mysqlTable("jobs", {
+  id: int("id").autoincrement().primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  employerId: int("employer_id")
+    .notNull()
+    .references(() => employers.id, { onDelete: "cascade" }),
+  description: text("description").notNull(),
+  tags: text("tags"),
+  minSalary: int("min_salary"),
+  maxSalary: int("max_salary"),
+  salaryCurrency: mysqlEnum("salary_currency", SALARY_CURRENCY),
+  salaryPeriod: mysqlEnum("salary_period", SALARY_PERIOD),
+  location: varchar("location", { length: 255 }),
+  jobType: mysqlEnum("job_type", JOB_TYPE),
+  workType: mysqlEnum("work_type", WORK_TYPE),
+  jobLevel: mysqlEnum("job_level", JOB_LEVEL),
+  experience: text("experience"),
+  minEducation: mysqlEnum("min_education", MIN_EDUCATION),
+  isFeatured: boolean("is_featured").default(false).notNull(),
+  expiresAt: date("expires_at"),
+  deletedAt: timestamp("deleted_at"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+});
+//!relation define of job and emplpyer
+export const jobsRelations = relations(jobs, ({ one }) => ({
+  // each job belongs to one employee
+  employer: one(employers, {
+    fields: [jobs.employerId],
+    references: [employers.id],
+  }),
+}));
 
 //!relation define
 export const usersRealtion = relations(users, ({ one, many }) => ({
